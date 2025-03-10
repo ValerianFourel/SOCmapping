faeb85ae30795f7d9eda1890a5aa988bfb829c9c
ValerianFourel <valerian.fourel@yahoo.com>
2025-03-11 00:24:58 +0100

    refactoring

--- find_large_files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-find . -type f -size +30M | while read file; do
-    # Remove ./ from the beginning of file path
-    file="${file#./}"
-    # Ignore if it's already in .gitignore
-    if ! grep -q "^${file}$" .gitignore; then
-        echo "$file" >> .gitignore
-    fi
-done
--- tmp.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-# Load the .npy file
-file_path = '/home/vfourel/SOCmapping/Data/Coordinates/YearlyValue/LST/2015/coordinates.npy'
-coordinates = np.load(file_path)
-
-# Display the first 5 entries
-print(coordinates[:5])
